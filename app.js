1b94fbe7863f7071395a3843d38146c4cfd9bcf9
emir91 <vranacemir@gmail.com>
2020-08-20 11:02:30 +0200

    Passing data to template

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/recipes", (req, res) => {
-  res.render("recipes");
+  const recipes = [
+    {
+      name: "Pizza Napolitana",
+      image:
+        "https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      name: "Homemade Burger",
+      image:
+        "https://images.unsplash.com/photo-1496930666207-e76e8253a950?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+
+    {
+      name: "Veggy Pasta",
+      image:
+        "https://images.unsplash.com/photo-1587206668283-c21d974993c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+  ];
+  res.render("recipes", { recipes: recipes });
 });
 
 const port = 5000;
