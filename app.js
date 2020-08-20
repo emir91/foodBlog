13e7ef903c0dd2b5bf3a2ebc76c48d83e50ae863
emir91 <vranacemir@gmail.com>
2020-08-20 12:40:39 +0200

    Add bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get("/", (req, res) => {
   res.render("landing");
